b0c68e201b22a3277a8b60c90e568d3678cb39ea
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-12-23 22:54:04 -0800

    the thing namer 3000

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,12 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        // This is a comment where I can describe what happens next.
+        // TODO: hi 
+        // Console.WriteLine("Hello, World!");
+        // Console.WriteLine("What is your name?");
+        // var name = Console.ReadLine();
+        // var currentDate = DateTime.Now;
+        // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        // Console.Write($"{Environment.NewLine}Press any key to exit...");
+        // Console.ReadKey(true);
+
+        Console.WriteLine("What kind of thing are we talkinmg about?");
+        string a = Console.ReadLine();
+        Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+        string b = Console.ReadLine();
+        string c = "Doom";
+        string d = "3000";
+        Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + '!');
+        // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        // Console.Write($"{Environment.NewLine}Press any key to exit...");
+        // Console.ReadKey(true);
     }
 }
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60154bae3ba0330ea9056c07e66ca1226b5be50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0dcfb4f269691e35ef53329962390c27e7b1cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
